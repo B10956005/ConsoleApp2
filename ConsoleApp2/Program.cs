5da8d3352545f1e0d5e01201f7cbf0b0d12579e3
user <user@DESKTOP-6JBEF5K>
2022-03-14 10:33:30 +0800

    加入專案檔案。

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入數字");
+            string s = Console.ReadLine();
+            int x = int.Parse(s);
+            if (x % 2 != 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if(x >=2 && x<=5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else if (x >= 6 && x <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                Console.WriteLine("Not Weird");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
